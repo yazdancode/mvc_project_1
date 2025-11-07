5afea478903ac27cb232ad35576595a43e9f168a
yazdancode <yshabanei@gmail.com>
2025-11-07 17:59:57 +0330

    config/app.php

--- config/app.php
+++ config/app.php
@@ -1,1 +1,20 @@
 <?php
+
+const APP_TITLE = 'mvc_project';
+const BASE_URL = 'http://localhost:8000';
+define('BASE_DIR', dirname(__DIR__) . "/");
+
+
+$temporary = str_replace(BASE_URL,'',explode('?', $_SERVER['REQUEST_URI'])[0]);
+$temporary === "/" ? $temporary = "" : $temporary = substr($temporary, 1);
+define('CURRENT_ROUTE', $temporary);
+
+
+global $routes;
+
+$routes = [
+    'get'=> [],
+    'post'=>[],
+    'put'=>[],
+    'delete'=>[]
+];

0d1a79d6f3d09cf897647e7a56ed12dfb691e5e0
yazdancode <yshabanei@gmail.com>
2025-11-09 11:25:19 +0330

    function get

--- routes/Web.php
+++ routes/Web.php
@@ -5,5 +5,5 @@ Route::get('', 'HomeController@index', 'index');
 Route::get('create', 'HomeController@create', 'create');
 Route::get('store', 'HomeController@store', 'store');
 Route::post('edit/{id}', 'HomeController@edit', 'edit');
-Route::put('update/{id}', 'HomeController@update', 'update');
-Route::delete('delete/{id}', 'HomeController@destroy', 'delete');
+Route::put('/update/{id}', 'HomeController@update', 'update');
+Route::delete('/delete/{id}', 'HomeController@destroy', 'delete');

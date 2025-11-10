ddd1c5b01aa432ed39cca3edfdd59d8f683ab24e
yazdancode <yshabanei@gmail.com>
2025-11-10 18:32:20 +0330

    404.php

--- system/Router/Routing.php
+++ system/Router/Routing.php
@@ -11,7 +11,7 @@ class Routing
 
     public function __construct()
     {
-        $this->current_route = explode('/', CURRENT_ROUTE);
+        $this->current_route = explode('/', trim(CURRENT_ROUTE, '/'));
         $this->method_field = $this->methodField();
         global $routes;
         $this->routes = $routes;
@@ -19,10 +19,35 @@ class Routing
 
     public function methodField()
     {
-        return $_SERVER['REQUEST_METHOD'];
+        $method_field = strtolower($_SERVER['REQUEST_METHOD']);
+
+        if ($method_field === 'post') {
+            if (isset($_POST['_method'])) {
+                $override = strtolower($_POST['_method']);
+                if (in_array($override, ['put', 'delete'])) {
+                    $method_field = $override;
+                }
+            }
+        }
+        return $method_field;
     }
 
     public function run()
     {
+    }
+
+    public function match()
+    {
+    }
+
+    public function compare()
+    {
+    }
+
+    public function error404($message = "صفحه مورد نظر یافت نشد.")
+    {
+        http_response_code(404);
+        include __DIR__ . DIRECTORY_SEPARATOR . 'View' .DIRECTORY_SEPARATOR .'404.php';
+        exit;
     }
 }
---
+++ system/Router/View/404.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Document</title>
+</head>
+<style>
+    body {
+        font-family: sans-serif;
+        background-color: #f8f8f8;
+        text-align: center;
+        padding-top: 100px;
+    }
+    .error_number {
+        font-size: 100px;
+        color: #e74c3c;
+        margin: 0;
+    }
+    .error_message {
+        font-size: 24px;
+        color: #555;
+    }
+</style>
+<body>
+<section class="container">
+    <section>
+        <h1 class="error_number">404</h1>
+        <p class="error_message">page not found</p>
+    </section>
+</section>
+</body>
+</html>

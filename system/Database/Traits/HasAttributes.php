063c78d7bdb72015bb603b92358456de238adbaa
yazdancode <yshabanei@gmail.com>
2025-11-16 23:33:51 +0330

    ff

--- system/Database/Traits/HasAttributes.php
+++ system/Database/Traits/HasAttributes.php
@@ -4,42 +4,57 @@ namespace System\Database\Traits;
 
 trait HasAttributes
 {
-    private function registerAttribute(): void
+    private function registerAttribute($object, string $attribute, $value): void
+    {
+        if ($this->inCastsAttributes($attribute)) {
+            $object->$attribute = $this->castDecodeValue($attribute, $value);
+        } else {
+            $object->$attribute = $value;
+        }
+    }
+
+    protected function arrayToAttributes(array $array, $object = null): object
+    {
+        if (!$object) {
+            $className = get_called_class();
+            $object = new $className();
+        }
+
+        foreach ($array as $attribute => $value) {
+            if ($this->inHiddenAttributes($attribute)) {
+                continue;
+            }
+            $this->registerAttribute($object, $attribute, $value);
+        }
+        return $object;
+    }
+
+    protected function arrayToObjects()
     {
 
     }
 
-    protected function arrayToAttributes(): void
+    protected function inHiddenAttributes()
     {
 
     }
 
-    protected function arrayToObjects(): void
+    private function inCastsAttributes()
     {
 
     }
 
-    protected function inHiddenAttributes(): void
+    private function castDecodeValue()
     {
 
     }
 
-    private function inCastsAttributes(): void
+    private function castEncodeValue()
     {
 
     }
 
-    private function castDecodeValue(): void
-    {
-
-    }
-
-    private function castEncodeValue(): void
-    {
-
-    }
-
-    private function arrayToCastEncodeValue(): void
+    private function arrayToCastEncodeValue()
     {
 
     }
